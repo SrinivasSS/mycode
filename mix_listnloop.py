12cfe3bf32cf3d5b363002ec3cdbcd0eb45b77cd
SrinivasSS <ssandhya@gmu.edu>
2019-06-24 11:20:46 -0400

    1st code

---
+++ mix_listnloop.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def main():
+    networklist = [['ios','10.0.0.1'],['ios','10.0.55.1'],['ios-xr','10.0.3.1'],['juno','10.0.10.1'],['eos','10.0.14.1']]
+
+    for driverandip in networklist:
+        print('SSH to ' +driverandip[1] + ' using driver ' +driverandip[0])
